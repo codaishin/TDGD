ac8b687ffb1be973f320c9030eb12b4af79cd00a
codaishin <codaishin@gmail.com>
2020-10-06 13:11:21 +0200

    CameraControler

---
+++ Assets/Scripts/Build/CameraControler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControler : MonoBehaviour
+{
+	public GameObject target;
+	public Vector3 offsetVector;
+
+	private void Update()
+	{
+		this.UpdateOffsetVector();
+		this.UpdateLookRotation();
+	}
+
+	private void UpdateOffsetVector()
+	{
+		this.transform.position =
+			this.target.transform.position +
+			this.offsetVector;
+	}
+
+	private void UpdateLookRotation()
+	{
+		this.transform.LookAt(this.target.transform);
+	}
+
+	public void SetOffsetFromCurrentPosition()
+	{
+		this.offsetVector =
+			this.transform.position -
+			this.target.transform.position;
+	}
+}
