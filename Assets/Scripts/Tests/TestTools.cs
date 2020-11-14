90a7e451e5cd9ed37b5c4c573168e1a052a33f41
codaishin <codaishin@gmail.com>
2020-11-14 14:57:44 +0100

    Added AssertAreEqual

--- Assets/Scripts/Tests/TestTools.cs
+++ Assets/Scripts/Tests/TestTools.cs
@@ -13,4 +13,27 @@ public class TestTools : MonoBehaviour
 			}
 		}
 	}
+
+	public static void AssertAreEqual(in Vector3 a, in Vector3 b)
+	{
+		if (a != b) {
+			throw new AssertionException(
+				$"Expected: ({a.x:F10}, {a.y:F10}, {a.z:F10})\n" +
+				$" But was: ({b.x:F10}, {b.y:F10}, {b.z:F10})"
+			);
+		}
+	}
+
+	public static void AssertAreEqual(in Vector3 a, in Vector3 b, in float delta)
+	{
+		if (Mathf.Abs(a.x - b.x) > delta ||
+		    Mathf.Abs(a.y - b.y) > delta ||
+		    Mathf.Abs(a.z - b.z) > delta) {
+			throw new AssertionException(
+				$"Expected: ({a.x:F10}, {a.y:F10}, {a.z:F10})\n" +
+				$" But was: ({b.x:F10}, {b.y:F10}, {b.z:F10})\n" +
+				$"Allowed delta: {delta:F10}"
+			);
+		}
+	}
 }
