d85609e4ab8132a57eddbadf6ee1421c73201611
codaishin <codaishin@gmail.com>
2020-11-15 11:25:07 +0100

    InputControlerTests: Added GetKeyMock

--- Assets/Scripts/Tests/InputControlerTests.cs
+++ Assets/Scripts/Tests/InputControlerTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System.Linq;
+using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
@@ -8,6 +9,20 @@ namespace Tests
 {
 	public class InputControlerTests : TestTools.BaseTestClass
 	{
+		private class GetKeyMock : GetKeyControler
+		{
+			public List<KeyCode> begin = new List<KeyCode>();
+			public List<KeyCode> stay = new List<KeyCode>();
+			public List<KeyCode> end = new List<KeyCode>();
 
+			public override
+			bool GetKeyDown(in KeyCode keyCode) => this.begin.Contains(keyCode);
+
+			public override
+			bool GetKey(in KeyCode keyCode) => this.stay.Contains(keyCode);
+
+			public override
+			bool GetKeyUp(in KeyCode keyCode) => this.end.Contains(keyCode);
+		}
 	}
 }
