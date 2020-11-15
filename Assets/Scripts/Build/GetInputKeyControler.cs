f9435f8ed182cf6c1faf18f13625d06969f51e35
codaishin <codaishin@gmail.com>
2020-11-15 11:07:59 +0100

    Added GetInputKeyControler

---
+++ Assets/Scripts/Build/GetInputKeyControler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public abstract class GetKeyControler : MonoBehaviour
+{
+	public abstract bool GetKeyDown(in KeyCode keyCode);
+	public abstract bool GetKey(in KeyCode keyCode);
+	public abstract bool GetKeyUp(in KeyCode keyCode);
+}
+
+public class GetInputKeyControler : GetKeyControler
+{
+	public override
+	bool GetKeyDown(in KeyCode keyCode) => Input.GetKeyDown(keyCode);
+
+	public override
+	bool GetKey(in KeyCode keyCode) => Input.GetKey(keyCode);
+
+	public override
+	bool GetKeyUp(in KeyCode keyCode) => Input.GetKeyUp(keyCode);
+}
