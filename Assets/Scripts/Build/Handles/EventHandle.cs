705ceb67eb8b2403dd56aad7b75d885bbfe2062c
codaishin <codaishin@gmail.com>
2020-11-18 13:08:18 +0100

    Added and Implemented EventHandle

---
+++ Assets/Scripts/Build/Handles/EventHandle.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventHandle : ScriptableObject
+{
+	public event Action OnRaise;
+
+	public void Raise() => this.OnRaise?.Invoke();
+}
---
+++ Assets/Scripts/Tests/Handles/EventHandleTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+	public class EventHandleTests : TestTools.BaseTestClass
+	{
+		[Test]
+		public void OnRaise()
+		{
+			var called = 0;
+			var eventHandle = ScriptableObject.CreateInstance<EventHandle>();
+			eventHandle.OnRaise += () => ++called;
+			eventHandle.Raise();
+
+			Assert.AreEqual(1, called);
+		}
+
+		[Test]
+		public void OnRaiseEmptyNoThrow()
+		{
+			var eventHandle = ScriptableObject.CreateInstance<EventHandle>();
+			Assert.DoesNotThrow(() => eventHandle.Raise());
+		}
+	}
+}
