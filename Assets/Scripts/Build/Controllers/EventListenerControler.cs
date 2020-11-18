02f2d47905cf43e8aaafcfd462f1ce0001b728f7
codaishin <codaishin@gmail.com>
2020-11-18 13:19:50 +0100

    EventListener: Base implementation

--- Assets/Scripts/Build/Controllers/EventListenerControler.cs
+++ Assets/Scripts/Build/Controllers/EventListenerControler.cs
@@ -1,8 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class EventListenerControler : MonoBehaviour
 {
+	public EventHandle eventHandle;
+	public UnityEvent onRaise = new UnityEvent();
 
+	private void Start()
+	{
+		this.eventHandle.OnRaise += this.onRaise.Invoke;
+	}
 }
--- Assets/Scripts/Tests/Controllers/EventListenerControlerTests.cs
+++ Assets/Scripts/Tests/Controllers/EventListenerControlerTests.cs
@@ -8,6 +8,34 @@ namespace Tests
 {
 	public class EventListenerControlerTests : TestTools.BaseTestClass
 	{
+		[UnityTest]
+		public IEnumerator OnRaiseNotNull()
+		{
+			var eventListenerCtrl = new GameObject("listener")
+				.AddComponent<EventListenerControler>();
+			eventListenerCtrl.eventHandle = ScriptableObject
+				.CreateInstance<EventHandle>();
 
+			yield return new WaitForEndOfFrame();
+
+			Assert.NotNull(eventListenerCtrl.onRaise);
+		}
+
+		[UnityTest]
+		public IEnumerator OnRaise()
+		{
+			var called = 0;
+			var eventListenerCtrl = new GameObject("listener")
+				.AddComponent<EventListenerControler>();
+			var eventHandle = ScriptableObject.CreateInstance<EventHandle>();
+			eventListenerCtrl.eventHandle = eventHandle;
+
+			yield return new WaitForEndOfFrame();
+
+			eventListenerCtrl.onRaise.AddListener(() => ++called);
+			eventHandle.Raise();
+
+			Assert.AreEqual(1, called);
+		}
 	}
 }
