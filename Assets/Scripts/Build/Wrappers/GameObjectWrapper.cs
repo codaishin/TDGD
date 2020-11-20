4fb5a1ba419ce6bf007a6136ee9af9d6f0dac5b7
codaishin <codaishin@gmail.com>
2020-11-20 13:51:33 +0100

    gameObjectWrapper: Base implementation

--- Assets/Scripts/Build/Wrappers/GameObjectWrapper.cs
+++ Assets/Scripts/Build/Wrappers/GameObjectWrapper.cs
@@ -4,5 +4,9 @@ using UnityEngine;
 
 public struct GameObjectWrapper
 {
+	public GameObject gameObject;
 
+	public GameObjectHandle handle;
+
+	public GameObject GameObject => this.gameObject ?? this.handle.GameObject;
 }
--- Assets/Scripts/Tests/Wrappers/GameObjectWrapperTests.cs
+++ Assets/Scripts/Tests/Wrappers/GameObjectWrapperTests.cs
@@ -8,6 +8,26 @@ namespace Tests
 {
 	public class GameObjectWrapperTests : TestTools.BaseTestClass
 	{
+		[Test]
+		public void GameObject()
+		{
+			var obj = new GameObject("obj");
+			var wrapper = new GameObjectWrapper();
+			wrapper.gameObject = obj;
 
+			Assert.AreSame(obj, wrapper.GameObject);
+		}
+
+		[Test]
+		public void GameObjectHandle()
+		{
+			var obj = new GameObject("obj");
+			var handle = ScriptableObject.CreateInstance<GameObjectHandle>();
+			var wrapper = new GameObjectWrapper();
+			handle.GameObject = obj;
+			wrapper.handle = handle;
+
+			Assert.AreSame(obj, wrapper.GameObject);
+		}
 	}
 }
