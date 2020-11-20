9b361f159c3fb4255824ec93abaf16c46dbd285e
codaishin <codaishin@gmail.com>
2020-11-20 14:43:49 +0100

    Updated DemoScene and Prefabs

--- Assets/Scripts/Build/Handles/GameObjectHandle.cs
+++ Assets/Scripts/Build/Handles/GameObjectHandle.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "ScriptableObjects/GameObjectHandle")]
 public class GameObjectHandle : ScriptableObject
 {
 	public GameObject GameObject { get; set; }
