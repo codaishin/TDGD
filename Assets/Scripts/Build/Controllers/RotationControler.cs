22bf72692dc623bdda45b969e2164ac4e0cda9e0
codaishin <codaishin@gmail.com>
2020-11-24 12:07:24 +0100

    RotationControler: Simple rotate

--- Assets/Scripts/Build/Controllers/RotationControler.cs
+++ Assets/Scripts/Build/Controllers/RotationControler.cs
@@ -4,5 +4,14 @@ using UnityEngine;
 
 public class RotationControler : MonoBehaviour
 {
+	public GameObjectWrapper around;
 
+	public void Rotate(float degrees)
+	{
+		this.transform.RotateAround(
+			this.around.GameObject.transform.position,
+			Vector3.up,
+			degrees
+		);
+	}
 }
--- Assets/Scripts/Tests/Controllers/RotationControlerTests.cs
+++ Assets/Scripts/Tests/Controllers/RotationControlerTests.cs
@@ -8,6 +8,20 @@ namespace Tests
 {
 	public class RotationControlerTests : TestTools.BaseTestClass
 	{
+		[Test]
+		public void Rotate()
+		{
+			var rotCtrl = new GameObject("rotater").AddComponent<RotationControler>();
+			var around = new GameObject("around");
 
+			rotCtrl.transform.position = new Vector3(1, 0, 0);
+			rotCtrl.around = new GameObjectWrapper(around);
+			rotCtrl.Rotate(-90);
+
+			TestTools.AssertAreEqual(
+				new Vector3(0, 0, 1),
+				rotCtrl.transform.position
+			);
+		}
 	}
 }
