997ebedd0ac197fe5a044461a8097b4ff1d51ac5
codaishin <codaishin@gmail.com>
2020-11-26 11:24:08 +0100

    RotationStaticLimiter: Implemented

--- Assets/Scripts/Build/Handles/RotationLimtHandle.cs
+++ Assets/Scripts/Build/Handles/RotationLimtHandle.cs
@@ -1,8 +1,28 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RotationLimtHandle : ScriptableObject
+public abstract class RotationLimtHandle : ScriptableObject
 {
+	public abstract
+	float Limit(in float angle, in Vector3 offset, in Vector3 axis);
+}
 
+public class RotationStaticLimitHandle : RotationLimtHandle
+{
+	public Vector3 levelPlaneNormal;
+	public float max;
+	public float min;
+
+	public override
+	float Limit(in float angle, in Vector3 offset, in Vector3 axis)
+	{
+		Vector3 level = Vector3.Cross(axis, this.levelPlaneNormal);
+		float currentAngle = Vector3.Angle(level, offset);
+		float targetAngle = currentAngle + angle;
+		if (targetAngle > this.max) return this.max - currentAngle;
+		if (targetAngle < this.min) return this.min - currentAngle;
+		return angle;
+	}
 }
--- Assets/Scripts/Tests/Handles/RotationLimitHandleTests.cs
+++ Assets/Scripts/Tests/Handles/RotationLimitHandleTests.cs
@@ -8,6 +8,69 @@ namespace Tests
 {
 	public class RotationLimitHandleTests : TestTools.BaseTestClass
 	{
+		[Test]
+		public void Limit()
+		{
+			var limitHandle = ScriptableObject
+				.CreateInstance<RotationStaticLimitHandle>();
 
+			limitHandle.levelPlaneNormal = Vector3.up;
+			limitHandle.max = 30;
+
+			var rotation = limitHandle.Limit(45, Vector3.forward, Vector3.right);
+
+			Assert.AreEqual(30, rotation);
+		}
+
+		[Test]
+		public void DontLimit()
+		{
+			var limitHandle = ScriptableObject
+				.CreateInstance<RotationStaticLimitHandle>();
+
+			limitHandle.levelPlaneNormal = Vector3.up;
+			limitHandle.max = 60;
+
+			var rotation = limitHandle.Limit(45, Vector3.forward, Vector3.right);
+
+			Assert.AreEqual(45, rotation);
+		}
+
+		[Test]
+		public void LimitFromAngle()
+		{
+			var limitHandle = ScriptableObject
+				.CreateInstance<RotationStaticLimitHandle>();
+
+			limitHandle.levelPlaneNormal = Vector3.up;
+			limitHandle.max = 60;
+
+			var rotation = limitHandle.Limit(
+				20,
+				new Vector3(0, 1, 1), // => offset of 45°
+				Vector3.right
+			); // => aiming for 65°
+
+			Assert.AreEqual(15, rotation);
+		}
+
+		[Test]
+		public void LimitMin()
+		{
+			var limitHandle = ScriptableObject
+				.CreateInstance<RotationStaticLimitHandle>();
+
+			limitHandle.levelPlaneNormal = Vector3.up;
+			limitHandle.max = 60;
+			limitHandle.min = 20;
+
+			var rotation = limitHandle.Limit(
+				-30,
+				new Vector3(0, 1, 1), // => offset of 45°
+				Vector3.right
+			); // => aiming for 15°
+
+			Assert.AreEqual(-25, rotation);
+		}
 	}
 }
