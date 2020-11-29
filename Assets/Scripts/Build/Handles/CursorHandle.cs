3e4de32bede8b08961167eb349f3f6bd2428ef9d
codaishin <codaishin@gmail.com>
2020-11-29 13:21:05 +0100

    CursorHandle: Added menu item

--- Assets/Scripts/Build/Handles/CursorHandle.cs
+++ Assets/Scripts/Build/Handles/CursorHandle.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "ScriptableObjects/CursorHandle")]
 public class CursorHandle : ScriptableObject
 {
 	public void SetVisibility(bool value) => Cursor.visible = value;
