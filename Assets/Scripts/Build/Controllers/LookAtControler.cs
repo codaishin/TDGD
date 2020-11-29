ab5b5c77c73d0a7ffc39537571c7735426c6f2a1
codaishin <codaishin@gmail.com>
2020-11-29 11:54:54 +0100

    LookAtControler: Inversion

--- Assets/Scripts/Build/Controllers/LookAtControler.cs
+++ Assets/Scripts/Build/Controllers/LookAtControler.cs
@@ -6,10 +6,14 @@ public class LookAtControler : MonoBehaviour
 {
 	public GameObjectWrapper target;
 	public GameObjectWrapper lookAt;
+	public bool invert;
 
 	public void Apply()
 	{
+		Vector3 targetPos = this.target.GameObject.transform.position;
+		Vector3 lookAtPos = this.lookAt.GameObject.transform.position;
+		Vector3 lookDir = lookAtPos - targetPos;
 		this.target.GameObject.transform
-			.LookAt(this.lookAt.GameObject.transform.position);
+			.LookAt(this.invert ? targetPos - lookDir : targetPos + lookDir);
 	}
 }
--- Assets/Scripts/Tests/Controllers/LookAtControlerTests.cs
+++ Assets/Scripts/Tests/Controllers/LookAtControlerTests.cs
@@ -29,5 +29,52 @@ namespace Tests
 				target.transform.rotation.eulerAngles
 			);
 		}
+
+		[Test]
+		public void LookAtInvert()
+		{
+			var target = new GameObject("target");
+			var lookAt = new GameObject("lookAt");
+			var lookCtrl = new GameObject("look").AddComponent<LookAtControler>();
+
+			lookAt.transform.position = Vector3.up;
+
+			lookCtrl.target = new GameObjectWrapper(target);
+			lookCtrl.lookAt = new GameObjectWrapper(lookAt);
+			lookCtrl.invert = true;
+
+			lookCtrl.Apply();
+
+			var expected = Quaternion.LookRotation(-Vector3.up);
+
+			TestTools.AssertAreEqual(
+				expected.eulerAngles,
+				target.transform.rotation.eulerAngles
+			);
+		}
+
+		[Test]
+		public void LookAtInvertFromNonOrigin()
+		{
+			var target = new GameObject("target");
+			var lookAt = new GameObject("lookAt");
+			var lookCtrl = new GameObject("look").AddComponent<LookAtControler>();
+
+			target.transform.position = Vector3.up * 2;
+			lookAt.transform.position = Vector3.up;
+
+			lookCtrl.target = new GameObjectWrapper(target);
+			lookCtrl.lookAt = new GameObjectWrapper(lookAt);
+			lookCtrl.invert = true;
+
+			lookCtrl.Apply();
+
+			var expected = Quaternion.LookRotation(Vector3.up);
+
+			TestTools.AssertAreEqual(
+				expected.eulerAngles,
+				target.transform.rotation.eulerAngles
+			);
+		}
 	}
 }
