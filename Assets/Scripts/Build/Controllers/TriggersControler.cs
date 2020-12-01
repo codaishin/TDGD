53745d3e1890e313b102fe4e6a3615d75ceeb2b9
codaishin <codaishin@gmail.com>
2020-12-01 15:19:15 +0100

    TriggersControler: Implemented

--- Assets/Scripts/Build/Controllers/TriggersControler.cs
+++ Assets/Scripts/Build/Controllers/TriggersControler.cs
@@ -2,7 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TriggersControler : MonoBehaviour
+public class TriggersControler : MonoBehaviour, IEnumerable<Collider>
 {
+	private Dictionary<int, Collider> colliders = new Dictionary<int, Collider>();
 
+	public IEnumerator<Collider> GetEnumerator()
+	{
+		foreach (Collider collider in this.colliders.Values) {
+			yield return collider;
+		}
+	}
+
+	IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+	private void OnTriggerEnter(Collider other)
+	{
+		this.colliders.Add(other.GetInstanceID(), other);
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		this.colliders.Remove(other.GetInstanceID());
+	}
 }
--- Assets/Scripts/Tests/Controllers/TriggersControlerTests.cs
+++ Assets/Scripts/Tests/Controllers/TriggersControlerTests.cs
@@ -8,6 +8,50 @@ namespace Tests
 {
 	public class TriggersControlerTests : TestTools.BaseTestClass
 	{
+		[Test]
+		public void Empty()
+		{
+			var triggersCtrl = new GameObject("obj")
+				.AddComponent<TriggersControler>();
 
+			Assert.IsEmpty(triggersCtrl);
+		}
+
+		[UnityTest]
+		public IEnumerator Trigger()
+		{
+			var other = new GameObject("other");
+			var triggersCtrl = new GameObject().AddComponent<TriggersControler>();
+			other.AddComponent<Rigidbody>().useGravity = false;
+			other.AddComponent<SphereCollider>();
+			triggersCtrl.gameObject.AddComponent<SphereCollider>().isTrigger = true;
+
+			yield return new WaitForEndOfFrame(); // Start()
+			yield return new WaitForEndOfFrame(); // Update()
+
+			CollectionAssert.AreEqual(
+				new Collider[] { other.GetComponent<Collider>() },
+				triggersCtrl
+			);
+		}
+
+		[UnityTest]
+		public IEnumerator TriggerLeave()
+		{
+			var other = new GameObject("other");
+			var triggersCtrl = new GameObject().AddComponent<TriggersControler>();
+			other.AddComponent<Rigidbody>().useGravity = false;
+			other.AddComponent<SphereCollider>();
+			triggersCtrl.gameObject.AddComponent<SphereCollider>().isTrigger = true;
+
+			yield return new WaitForEndOfFrame(); // Start()
+			yield return new WaitForEndOfFrame(); // Update()
+
+			other.transform.position = new Vector3(10, 0, 0);
+
+			yield return new WaitForEndOfFrame();
+
+			Assert.IsEmpty(triggersCtrl);
+		}
 	}
 }
