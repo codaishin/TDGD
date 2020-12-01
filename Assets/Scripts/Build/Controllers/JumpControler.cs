bfcfa9e17669badcb458cbb01d1c9d50c99dadda
codaishin <codaishin@gmail.com>
2020-12-01 15:49:01 +0100

    JumpControler: Implementation

--- Assets/Scripts/Build/Controllers/JumpControler.cs
+++ Assets/Scripts/Build/Controllers/JumpControler.cs
@@ -1,8 +1,29 @@
-﻿using System.Collections;
+﻿using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class JumpControler : MonoBehaviour
 {
+	private Rigidbody rb;
+	private IEnumerable<Collider> triggers;
 
+	public GameObjectWrapper targetRigidbody;
+	public GameObjectWrapper targetTriggers;
+	public Vector3 direction = Vector3.up;
+	public float strength = 1;
+
+	private void Start()
+	{
+		this.rb = this.targetRigidbody
+			.AssertComponent<Rigidbody>();
+		this.triggers = this.targetTriggers
+			.AssertComponent<IEnumerable<Collider>>();
+	}
+
+	public void Jump()
+	{
+		if (this.triggers.Any()) {
+			this.rb.velocity += this.direction.normalized * this.strength;
+		}
+	}
 }
--- Assets/Scripts/Tests/Controllers/JumpControlerTests.cs
+++ Assets/Scripts/Tests/Controllers/JumpControlerTests.cs
@@ -8,6 +8,123 @@ namespace Tests
 {
 	public class JumpControlerTests : TestTools.BaseTestClass
 	{
+		private class TriggersMock : MonoBehaviour, IEnumerable<Collider>
+		{
+			public List<Collider> colliders = new List<Collider>();
 
+			public IEnumerator<Collider> GetEnumerator()
+				=> this.colliders.GetEnumerator();
+
+			IEnumerator IEnumerable.GetEnumerator()
+				=> this.GetEnumerator();
+		}
+
+		[UnityTest]
+		public IEnumerator Jump()
+		{
+			var targetRb = new GameObject("target").AddComponent<Rigidbody>();
+			var jumpCtrl = new GameObject("jump").AddComponent<JumpControler>();
+			var triggerMock = new GameObject("triggers").AddComponent<TriggersMock>();
+
+			jumpCtrl.targetRigidbody = new GameObjectWrapper(targetRb.gameObject);
+			jumpCtrl.targetTriggers = new GameObjectWrapper(triggerMock.gameObject);
+			targetRb.useGravity = false;
+
+			yield return new WaitForEndOfFrame(); // Start
+
+			triggerMock.colliders
+				.Add(new GameObject("collider").AddComponent<SphereCollider>());
+
+			jumpCtrl.Jump();
+
+			TestTools.AssertAreEqual(Vector3.up, targetRb.velocity);
+		}
+
+		[UnityTest]
+		public IEnumerator NoJumpWhenNotTriggered()
+		{
+			var targetRb = new GameObject("target").AddComponent<Rigidbody>();
+			var jumpCtrl = new GameObject("jump").AddComponent<JumpControler>();
+			var triggerMock = new GameObject("triggers").AddComponent<TriggersMock>();
+
+			jumpCtrl.targetRigidbody = new GameObjectWrapper(targetRb.gameObject);
+			jumpCtrl.targetTriggers = new GameObjectWrapper(triggerMock.gameObject);
+			targetRb.useGravity = false;
+
+			yield return new WaitForEndOfFrame(); // Start
+
+			jumpCtrl.Jump();
+
+			TestTools.AssertAreEqual(Vector3.zero, targetRb.velocity);
+		}
+
+		[UnityTest]
+		public IEnumerator JumpStrength()
+		{
+			var targetRb = new GameObject("target").AddComponent<Rigidbody>();
+			var jumpCtrl = new GameObject("jump").AddComponent<JumpControler>();
+			var triggerMock = new GameObject("triggers").AddComponent<TriggersMock>();
+
+			jumpCtrl.strength = 2;
+			jumpCtrl.targetRigidbody = new GameObjectWrapper(targetRb.gameObject);
+			jumpCtrl.targetTriggers = new GameObjectWrapper(triggerMock.gameObject);
+			targetRb.useGravity = false;
+
+			yield return new WaitForEndOfFrame(); // Start
+
+			triggerMock.colliders
+				.Add(new GameObject("collider").AddComponent<SphereCollider>());
+
+			jumpCtrl.Jump();
+
+			TestTools.AssertAreEqual(Vector3.up * 2, targetRb.velocity);
+		}
+
+		[UnityTest]
+		public IEnumerator JumpDirection()
+		{
+			var targetRb = new GameObject("target").AddComponent<Rigidbody>();
+			var jumpCtrl = new GameObject("jump").AddComponent<JumpControler>();
+			var triggerMock = new GameObject("triggers").AddComponent<TriggersMock>();
+
+			jumpCtrl.direction = new Vector3(-1, 0, 0);
+			jumpCtrl.targetRigidbody = new GameObjectWrapper(targetRb.gameObject);
+			jumpCtrl.targetTriggers = new GameObjectWrapper(triggerMock.gameObject);
+			targetRb.useGravity = false;
+
+			yield return new WaitForEndOfFrame(); // Start
+
+			triggerMock.colliders
+				.Add(new GameObject("collider").AddComponent<SphereCollider>());
+
+			jumpCtrl.Jump();
+
+			TestTools.AssertAreEqual(new Vector3(-1, 0, 0), targetRb.velocity);
+		}
+
+		[UnityTest]
+		public IEnumerator JumpDirectionNormalized()
+		{
+			var targetRb = new GameObject("target").AddComponent<Rigidbody>();
+			var jumpCtrl = new GameObject("jump").AddComponent<JumpControler>();
+			var triggerMock = new GameObject("triggers").AddComponent<TriggersMock>();
+
+			jumpCtrl.direction = new Vector3(0, 1, 1);
+			jumpCtrl.targetRigidbody = new GameObjectWrapper(targetRb.gameObject);
+			jumpCtrl.targetTriggers = new GameObjectWrapper(triggerMock.gameObject);
+			targetRb.useGravity = false;
+
+			yield return new WaitForEndOfFrame(); // Start
+
+			triggerMock.colliders
+				.Add(new GameObject("collider").AddComponent<SphereCollider>());
+
+			jumpCtrl.Jump();
+
+			TestTools.AssertAreEqual(
+				new Vector3(0, Mathf.Sqrt(2) / 2, Mathf.Sqrt(2) / 2),
+				targetRb.velocity
+			);
+		}
 	}
 }
